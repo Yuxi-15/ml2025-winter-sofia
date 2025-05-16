d4466be6084d8d7e28aea40708de04c98bed8bb9
Yuxi15 <yuxihong0515@gmail.com>
2025-05-16 14:50:53 -0700

    update name

--- src/module6_call.py
+++ src/module6_call.py
@@ -1,24 +1,25 @@
-from module5_mod import NumberSearcher
+from module6_knn_regr import KNNRegression
+
 
 def main():
-    searcher = NumberSearcher()
+    regression = KNNRegression()
 
     while True:
-        count = searcher.get_positive_int("How many numbers do you want to enter? ")
-        searcher.read_numbers(count)
+        count = regression.get_positive_int("How many numbers do you want to enter? ")
+        regression.read_numbers(count)
 
-        k = searcher.get_positive_int("Enter k (number of neighbors): ")
-        searcher.k = k
+        k = regression.get_positive_int("Enter k (number of neighbors): ")
+        regression.k = k
 
-        x = searcher.get_float("Enter the number to predict Y for (X value): ")
+        x = regression.get_float("Enter the number to predict Y for (X value): ")
 
         if k > count:
             print(f"Error: k ({k}) cannot be greater than N ({count}).")
         else:
-            result = searcher.search_number(x)
+            result = regression.search_number(x)
             print(f"Predicted Y value for X={x} using {k}-NN regression is: {result}")
 
-        if searcher.ask_exit():
+        if regression.ask_exit():
             print("Exiting the program.")
             break
 
